7b6d4c122a8685d4a653efb560e5eaa8de3c3d14
nadia0ostapyshyn <nadia0ostapyshyn@gmail.com>
2023-12-24 17:35:37 +0200

    add router and a link to a game

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import Homepage from "./Homepage/Homepage";
+import Homepage from "./pages/Homepage/Homepage";
+import Hangman from "./pages/Hangman/Hangman";
 
 function App() {
-  return <Homepage />;
+  return (
+    <Routes>
+      <Route path="/" element={<Homepage />} />
+      <Route path="/hangman" element={<Hangman />} />
+    </Routes>
+  );
 }
 
 export default App;
