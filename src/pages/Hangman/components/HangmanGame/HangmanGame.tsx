15fcc7fdee6c6780997fd5bbed1456d291b2a7b0
nadia0ostapyshyn <nadia0ostapyshyn@gmail.com>
2024-03-01 19:19:47 +0200

    add back to home button

--- src/pages/Hangman/components/HangmanGame/HangmanGame.tsx
+++ src/pages/Hangman/components/HangmanGame/HangmanGame.tsx
@@ -9,6 +9,7 @@ import useHangmanGame from "../../hooks/useHangmanGame";
 import useHint from "../../hooks/useHint";
 import { useEffect, useState } from "react";
 import getWordsFromLocalStorage from "../../../../services/getWordsFromLocalStorage";
+import { useNavigate } from "react-router-dom";
 
 const TOTAL_OF_INCORRECT_GUESSES = 7;
 
@@ -17,6 +18,8 @@ type HangmanGameProps = {
 };
 
 const HangmanGame: React.FC<HangmanGameProps> = () => {
+  const navigate = useNavigate();
+
   const { isModalOpen, modalText, closeModal, openModal } = useModal();
 
   const [wordToGuess, setWordToGuess] = useState<string[]>([]);
@@ -104,6 +107,17 @@ const HangmanGame: React.FC<HangmanGameProps> = () => {
           <Keyboard clickHandler={letterClickHandler} />
         </div>
       </div>
+      <button className="button button--home" onClick={() => navigate("/")}>
+        <svg fill="#ffffff" height="0.7em" width="0.7em" viewBox="0 0 330 330">
+          <g id="SVGRepo_iconCarrier">
+            <path
+              id="XMLID_92_"
+              d="M111.213,165.004L250.607,25.607c5.858-5.858,5.858-15.355,0-21.213c-5.858-5.858-15.355-5.858-21.213,0.001 l-150,150.004C76.58,157.211,75,161.026,75,165.004c0,3.979,1.581,7.794,4.394,10.607l150,149.996 C232.322,328.536,236.161,330,240,330s7.678-1.464,10.607-4.394c5.858-5.858,5.858-15.355,0-21.213L111.213,165.004z"
+            ></path>
+          </g>
+        </svg>
+        <span className="modal__buttons-text">Home</span>
+      </button>
       <Modal isOpen={isModalOpen} onClose={closeModal}>
         {modalText}
       </Modal>
